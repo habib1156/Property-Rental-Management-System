06fb9426bc957da3fc7087e6e2c313aca98cb4f7
habib1156 <163901813+habib1156@users.noreply.github.com>
2024-09-20 22:02:15 +0600

    Add files via upload

---
+++ include/Rent.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    
+    <link rel="stylesheet" href="./include/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./include/css/all.min.css">
+    <link rel="stylesheet" href="./include/css/fontawesome.min.css">
+    <link rel="stylesheet" href="./include/css/index.css">
+    <link rel="stylesheet" href="./include/css/Rent.css">
+<title> rent</title>
+</head>
+  <!---------------------------------------------------------------------------------------------------------------------->
+    
+<div class="header-top-right">
+  <div class="main">   
+    
+        <ul class="yat">
+      <h1 class="title "><i class="fa-solid fa-tent" style="color: #ffffff;"></i> Property Rental</h1>
+         
+        
+      </ul>    
+  </div>
+</div>
+
+  
+  <div class="header-bottom">
+      <div class="main">   
+          <ul class="yati">
+
+             <li><a href="./index.php"> Home</a></li> 
+             <li><a href="./aboutus.php"> About </a></li>
+            <li><a href="./Rent.php"> Property</a></li> 
+              <li><a href="./contact.php"> Contact Us</a></li>
+              <li><a href="./login.php"><i class="fa-solid fa-user"></i> Login</a></li> 
+              
+          </ul>    
+      </div>
+  </div>
+  
+
+    
+    
+<body>
+    <!---------------------------------------------------------------------------------------------------------------------->
+    
+    <div class="container">
+      
+      <div class="border">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="postpic">
+              <a href="./details.php" ><img src="./include/img/1.1.jpg" width="450px"></a>
+                 
+           </div>
+          </div>
+          <div class="col-md-6">
+            <div class="texttt">
+              <h2> BDT 1.35 core</h2><br>
+              <h4> Apertment </h4> <h4><i class="fa-solid fa-bed">1</i> <i class= "fa-solid fa-bath">1</i></h4>
+             <br><p >
+              <b>  Ready Flat Of 1265 Sq Ft Is Now Up For Sale In Baridhara Dohs </b></p>
+            
+            <div class="buttonone"><p>
+              <button class="buttontwo"> Email</button> 
+              <button class="buttontwo"> Contact Us</button>
+              <br>
+              <a href="./details.php"><p>See More</p></a>
+              </p>
+         </div>
+          </div>
+          </div>
+        </div>
+        </div>
+        <div class="border">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="postpic">
+                <a href="./details.php" ><img src="./include/img/1.2.jpg" width="450px"></a>
+                   
+             </div>
+            </div>
+            <div class="col-md-6">
+              <div class="texttt">
+                <h2> BDT 1.35 core</h2><br>
+                <h4> Apertment </h4> <h4><i class="fa-solid fa-bed">1</i> <i class= "fa-solid fa-bath">1</i></h4>
+               <br><p >
+                <b>  Ready Flat Of 1265 Sq Ft Is Now Up For Sale In Baridhara Dohs </b></p>
+              
+              <div class="buttonone"><p>
+                <button class="buttontwo"> Email</button> 
+                <button class="buttontwo"> Contact Us</button>
+                <br>
+                <a href="./details.php"><p>See More</p></a>
+                </p>
+           </div>
+            </div>
+            </div>
+          </div>
+          </div>
+          <div class="border">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="postpic">
+                  <a href="./details.php" ><img src="./include/img/1.jpg" width="450px"></a>
+                     
+               </div>
+              </div>
+              <div class="col-md-6">
+                <div class="texttt">
+                  <h2> BDT 1.35 core</h2><br>
+                  <h4> Apertment </h4> <h4><i class="fa-solid fa-bed">1</i> <i class= "fa-solid fa-bath">1</i></h4>
+                 <br><p >
+                  <b>  Ready Flat Of 1265 Sq Ft Is Now Up For Sale In Baridhara Dohs </b></p>
+                
+                <div class="buttonone"><p>
+                  <button class="buttontwo"> Email</button> 
+                  <button class="buttontwo"> Contact Us</button>
+                  <br>
+                  <a href="./details.php"><p>See More</p></a>
+                  </p>
+             </div>
+              </div>
+              </div>
+            </div>
+            </div>
+            <div class="border">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="postpic">
+                    <a href="./details.php" ><img src="./include/img/1.2.jpg" width="450px"></a>
+                       
+                 </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="texttt">
+                    <h2> BDT 1.35 core</h2><br>
+                    <h4> Apertment </h4> <h4><i class="fa-solid fa-bed">1</i> <i class= "fa-solid fa-bath">1</i></h4>
+                   <br><p >
+                    <b>  Ready Flat Of 1265 Sq Ft Is Now Up For Sale In Baridhara Dohs </b></p>
+                  
+                  <div class="buttonone"><p>
+                    <button class="buttontwo"> Email</button> 
+                    <button class="buttontwo"> Contact Us</button>
+                    <br>
+                    <a href="./details.php"><p>See More</p></a>
+                    </p>
+               </div>
+                </div>
+                </div>
+              </div>
+              </div>
+              <div class="border">
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="postpic">
+                      <a href="./details.php" ><img src="./include/img/1.1.jpg" width="450px"></a>
+                         
+                   </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="texttt">
+                      <h2> BDT 1.35 core</h2><br>
+                      <h4> Apertment </h4> <h4><i class="fa-solid fa-bed">1</i> <i class= "fa-solid fa-bath">1</i></h4>
+                     <br><p >
+                      <b>  Ready Flat Of 1265 Sq Ft Is Now Up For Sale In Baridhara Dohs </b></p>
+                    
+                    <div class="buttonone"><p>
+                      <button class="buttontwo"> Email</button> 
+                      <button class="buttontwo"> Contact Us</button>
+                      <br>
+                      <a href="./details.php">See More</a>
+                      </p>
+                 </div>
+                  </div>
+                  </div>
+                </div>
+                </div>
+    </div>
+
+<!--------------------------------------------------------------------------------------------------------------------------------->
+
+<br>
+
+
+
+
+
+
+
+
+
+     <!-----------------------------------------------S-->
+      <footer class="footer">
+                     
+                  <div class="main">   
+                     
+                      <ul class="yate">
+                         <li><a href="./aboutus.php"> ABOUT US</a></li> 
+                         <li><a href="./contact.php"> CONTACT US</a></li> 
+                         <li><a href="#"> CAREERS</a></li>
+                             
+                         <p><i class="fa-regular fa-copyright"></i> 2023 Property Rental</p>
+              </ul>
+                  
+                  </div>
+                  <div class="main">   
+                     
+                      <ul class="yate">
+                  <li><a href="https://www.facebook.com"><i class="fa-brands fa-facebook"></i></a></li>
+                  <li><a href="https://www.twitter.com"><i class="fa-brands fa-twitter"></i></a> </li>
+                  <li><a href="https://www.pinterest.com"><i class="fa-brands fa-pinterest"></i></a> </li>
+                  <li><a href="https://www.instragram.com/"><i class="fa-brands fa-instagram"></i></a> </li>
+                  <li><a href="https://www.youtube.com"> <i class="fa-brands fa-youtube"></i></a> </li>
+              </ul>
+                  
+          </div>
+      
+      
+         </footer> 
+         
+<!----------------------------------------------------------------------------------------------------------------------> 
+      <script src="./include/js/jquery-3.7.0.js"></script>
+      <script src="./include/js/bootstrap.bundle.js"></script>
+</body>
+</html>
---
+++ include/project.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./include/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./include/css/all.min.css">
+    <link rel="stylesheet" href="./include/css/fontawesome.min.css">
+    <link rel="stylesheet" href="./include/css/index.css">
+    <link rel="stylesheet" href="./include/css/project.css">
+    <title>Projects</title>
+</head>
+
+<!------------------------------------------------------------------------------------------------------------------->
+<!------------------------------------------------------------------------------------------------------------------->
+
+
+
+<div class="header-top-right">
+    <div class="main">   
+          <ul class="yati">
+        
+            <li><a href="./login.php"><i class="fa-solid fa-user"></i> Login</a></li> 
+          
+        </ul>    
+    </div>
+</div>
+
+
+
+<div class="header-bottom">
+    <div class="main">   
+       
+        <ul class="yati">
+            
+          
+           <li><a href="./index.php"> Home</a></li> 
+           <li><a href="./aboutus.php"> About </a></li>
+          <li><a href="./Rent.php"> Property/a></li> 
+            <li><a href="./contact.php"> Contact Us</a></li> 
+            
+        </ul>    
+    </div>
+</div>
+
+
+<!------------------------------------------------------------------------------------------------------------------->
+<!------------------------------------------------------------------------------------------------------------------->
+<body>
+    <div>
+        <div class="text">
+            <h1>Popular Dhaka Neighborhoods</h1><br>
+            <!------------------------------------------------------------------------------------------------------------>
+        <div>
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item" role="presentation">
+                  <button class="nav-link active" id="sale-tab" data-bs-toggle="tab" data-bs-target="#sale-tab-pane" type="button" role="tab" aria-controls="sale-tab-pane" aria-selected="true"><h4>For Sale</h4></button>
+                </li>
+                <li class="nav-item" role="presentation">
+                  <button class="nav-link" id="rent-tab" data-bs-toggle="tab" data-bs-target="#rent-tab-pane" type="button" role="tab" aria-controls="rent-tab-pane" aria-selected="false"><h4>To Rent</h4></button>
+                </li>
+              </ul>
+              <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="sale-tab-pane" role="tabpanel" aria-labelledby="sale-tab" tabindex="0">
+                    
+            
+            <br>
+            <div class="row">
+                <div class="col-6 col-md-4"><u><b><a href="./Rent.php" >Dhaka Appartments</a></b></u><br>
+                    <li>Bashundhara R-A</li>
+                    <li>Uttara </li>
+                    <li>Mirpur</li>
+                    <li>Gulshan </li>
+                    <li>Dhanmondi</li>
+                </div>
+                <div class="col-6 col-md-4"><u><b>Chottogram Appartments</b></u><br>
+                    <li>Bayazid</li>
+                    <li> Double Mooring </li>
+                    <li>Khulshi </li>
+                    <li>16 No. Chawk Bazaar Ward</li>
+                    <li>4 No Chandgaon Ward</li>
+                </div>
+                <div class="col-6 col-md-4"><b>Chattogram Popular Location</b>
+                    
+                    <li>Bayazid</li>
+                    <li>Khulshi </li>
+                    <li>4 No Chandgaon Ward</li>
+                    <li>Jamal Khan</li>
+                    <li>Bakalia</li>
+                    
+                </div>
+            </div>
+        <br>
+            <div class="row">
+                <div class="col-6 col-md-4"><u><b>Dhaka offices</b></u><br>
+                    <li>Motijheel</li>
+                    <li>Mohammadpur</li>
+                    <li>Kalabagan</li>
+                    <li>Dhanmondi</li>
+                    <li>Badda</li>
+                </div>
+                <div class="col-6 col-md-4"><b>Dhaka Popular Locations</b><br>
+                    <li><a>Bashundhara R-A</a></li>
+                    <li><a>Uttara</a></li>
+                    <li><a>Mirpur</a></li>
+                    <li><a>Gulshan</a></li>
+                    <li><a>Dhanmondi</a></li>
+                </div>
+                <div class="col-6 col-md-4"><u><b>Dhaka Commercial</b></u>
+                    
+                    <li>Motijheel</li>
+                    <li>Dhanmondi</li>
+                    <li>Badda</li>
+                    <li>Tejgaon</li>
+                    <li>Uttara</li>
+                    
+                </div>
+            </div>
+              </div>
+        
+        
+            
+            <div class="tab-pane fade" id="rent-tab-pane" role="tabpanel" aria-labelledby="rent-tab" tabindex="0"> 
+                
+            <br>
+            <div class="row">
+                <div class="col-6 col-md-4"><u><b><a href="./Rent.php" >Dhaka Appartments</a></b></u><br>
+                    <li>Bashundhara R-A</li>
+                    <li>Uttara </li>
+                    <li>Mirpur</li>
+                    <li>Gulshan </li>
+                    <li>Dhanmondi</li>
+                </div>
+                <div class="col-6 col-md-4"><u><b>Chottogram Appartments</b></u><br>
+                    <li>Khulshi</li>
+                    <li> Halishahar</li>
+                    <li>South Khulshi </li>
+                    <li>Kazir Dewri</li>
+                    <li>15.No Bagmoniram Ward</li>
+                </div>
+                <div class="col-6 col-md-4"><b>Chattogram Popular Location</b>
+                    
+                    <li>Halishahar</li>
+                    <li>Khulshi </li>
+                    <li>Kazir Dewri</li>
+                    <li>Panchalaish</li>
+                    
+                    
+                </div>
+            </div>
+        <br>
+            <div class="row">
+                <div class="col-6 col-md-4"><u><b>Dhaka offices</b></u><br>
+                    <li>Uttara</li>
+                    <li>Motijheel</li>
+                    <li>Tejgaon</li>
+                    <li>Banani</li>
+                    <li>Mirpur</li>
+                    
+                </div>
+                <div class="col-6 col-md-4"><b>Dhaka Popular Locations</b><br>
+                    <li><a>Bashundhara R-A</a></li>
+                    <li><a>Uttara</a></li>
+                    <li><a>Mirpur</a></li>
+                    <li><a>Gulshan</a></li>
+                    <li><a>Dhanmondi</a></li>
+                </div>
+                <div class="col-6 col-md-4"><u><b>Dhaka Commercial</b></u>
+                    <li>Uttara</li>
+                    <li>Gulshan</li>
+                    <li>Banani</li>
+                    <li>Dhanmondi</li>
+                    <li>Mohammadpur</li>
+                    
+                    
+                </div>
+            </div>
+        <br>
+            <div class="row">
+                <p><b> Popular Property for Sale searches in Bangladesh</b></p>
+                <div class="col-6 col-md-4">
+                    <li>Uttara Properties for sale</li>
+                    <li>Badda Properties for sale</li>
+                    <li>Bayazid Properties for sale</li>
+                    
+                </div>
+                <div class="col-6 col-md-4">
+                    <li>Dhanmondi Shops for sale</li>
+                    <li>Chottogram Shops for sale</li>
+                    <li>Uttara Open Floor for sale</li>
+                </div>
+                <div class="col-6 col-md-4">
+                    
+                    <li>Dhaka Duplex for sale</li>
+                    <li>Dhaka Plots for sale</li>
+                    <li>Dhaka office Appartments for sale</li>
+                    
+                </div>
+            </div>
+        
+            </div>
+        
+              </div>
+        
+            </div>
+        </div>
+    </div>
+    <!------------------------------------------------------------------------------------------------------------------->
+    <!------------------------------------------------------------------------------------------------------------------->
+    
+<!------------------------------------------------------------------------------------------------------------------->
+<!------------------------------------------------------------------------------------------------------------------->
+<footer class="footer">
+               
+    <div class="main">   
+       
+        <ul class="yate">
+           <li><a href="./aboutus.php"> ABOUT US</a></li> 
+           <li><a href="./contact.php"> CONTACT US</a></li> 
+           <li><a href="#"> CAREERS</a></li>
+         
+     
+           <p><i class="fa-regular fa-copyright"></i> 2015-2023 Property Rental</p>
+</ul>
+    
+    </div>
+    <div class="main">   
+       
+        <ul class="yate">
+    <li><a href="https://www.facebook.com"><i class="fa-brands fa-facebook"></i></a></li>
+    <li><a href="https://www.twitter.com"><i class="fa-brands fa-twitter"></i></a> </li>
+    <li><a href="https://www.pinterest.com"><i class="fa-brands fa-pinterest"></i></a> </li>
+    <li><a href="https://www.instragram.com/"><i class="fa-brands fa-instagram"></i></a> </li>
+    <li><a href="https://www.youtube.com"> <i class="fa-brands fa-youtube"></i></a> </li>
+</ul>
+    
+</div>
+
+
+</footer>
+
+ <!------------------------------------------------------------------------------------------------------------------->
+<!------------------------------------------------------------------------------------------------------------------->
+
+   <script src="./include/js/jquery-3.7.0.js"></script>
+   <script src="./include/js/bootstrap.bundle.js"></script>
+
+   
+   <!------------------------------------------------------------------------------------------------------------------->
+   <!------------------------------------------------------------------------------------------------------------------->
+</body>
+</html>
